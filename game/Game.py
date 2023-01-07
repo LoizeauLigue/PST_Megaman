4be8b8f2f3afb6d40eb6e229d44a1a57ca22b202
Nitraxenius <ribeiro@et.esiea.fr>
2023-01-07 15:07:17 +0100

    caméra map j'ai enlevé tes colisions

--- game/Game.py
+++ game/Game.py
@@ -1,9 +1,9 @@
 import pygame
 from Projectile import projectile
 from Player import Player
-from Stage import Stage
+
 
 
 class Game:
     def __init__(self):
-        self.player = Player((100, 300))
+        self.player = Player((0, 0))
--- game/Stage.py
+++ game/Stage.py
@@ -1,20 +1,26 @@
 import pygame
-
-
-class Stage(pygame.sprite.Sprite):
-    def __init__(self, pos):
-        self.fillbackground = pygame.Surface(pos)
-        color = (244, 164, 96)
-        self.fillbackground.fill(color)
-        self.background = pygame.image.load('Resources/background/waterfall_1.png')
-        self.background_rect = self.background.get_rect()
-        self.background_x = 50
-        self.background_y = 50
-        super().__init__()
-
-    def display(self, screen):
-        screen.blit(self.fillbackground, (0, 0))
-        screen.blit(self.background, self.background_rect)
-
-    def collision(self):
-        print(1)
+from Player import Player
+from Tile import Tile
+from Setting import title_size
+from Game import Game
+class Stage:
+    def __init__(self,level_data, surface):
+        self.display_surface = surface
+        self.setup_level(level_data)
+        self.world_shift = 0
+    def setup_level(self,layout):
+        self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+        for row_index,row in enumerate(layout):
+            for col_index,cell in enumerate(row):
+                x = col_index * title_size
+                y = row_index * title_size
+                if cell == 'X':
+                    self.tiles.add(Tile((x,y),title_size))
+                if cell == 'P':
+                    self.player = Player((x,y))
+                    self.player.rect.x = x
+                    self.player.rect.y = y
+    def run(self):
+       self.tiles.update(self.world_shift)
+       self.tiles.draw(self.display_surface)
